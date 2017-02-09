da17a2d7835f35287c1e161fefe8295e70406203
smaislin186 <samantha.maislin@gmail.com>
2017-02-08 21:47:46 -0500

    Added google fonts

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
     
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
     <!-- Local style sheet -->
     <link rel="stylesheet" href="/static/style.css"/> 
     
@@ -22,9 +24,8 @@
     
         <p>
             <img src="/images/Pats.jpg" alt="Samantha Maislin picture" class="leftalign">
-
             <h2>About Me</h2>
-                I am a Product Manager/Product Owner for a financial software company. We make and sell software for
+             <div class="text">I am a Product Manager/Product Owner for a financial software company. We make and sell software for
                 financial institutions to help them project income and meet regulatory requirements. I have always been interested in Computer
                 Science since I took a year's worth of Java in college (over 10 years ago). At my company, I have the unique experience to work directly
                 with the developers and even am able to debug parts of the code! We are looking to move from a .Net Desktop application to 
@@ -33,10 +34,10 @@
                 my father, who grew up in the Bronx, to root for the Yankees. However, he was inneffective of making me like the Giants, GO PATS!
                 I love In-n-Out and am engaged to the most awesomest man ever. We are planning our wedding and counting down the days till September 
                 23rd! Oh and I can't forget our two cats.  They're the best, too.
+            </div>
         <p>
         <p>
             <h3>Words of Wisdom</h3>       
-                
             <div class="quotes">
                 <blockquote>
                     <?php  
